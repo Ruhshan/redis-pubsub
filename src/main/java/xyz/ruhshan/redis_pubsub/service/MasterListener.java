861e0416c094a334daf6654d7e9a0e926c78487c
Ruhshan <5312918+Ruhshan@users.noreply.github.com>
2025-01-18 01:31:31 +0600

    Some refactoring

--- src/main/java/xyz/ruhshan/redis_pubsub/service/MasterListener.java
+++ src/main/java/xyz/ruhshan/redis_pubsub/service/MasterListener.java
@@ -1,6 +1,5 @@
 package xyz.ruhshan.redis_pubsub.service;
 
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
@@ -8,10 +7,7 @@ import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Component;
 import xyz.ruhshan.redis_pubsub.redis_listener.RedisListenerDetails;
 
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import java.util.Optional;
 
 import static xyz.ruhshan.redis_pubsub.redis_listener.ListenerScanner.getListeners;
 
@@ -24,29 +20,28 @@ public class MasterListener implements MessageListener {
         this.objectMapper = objectMapper;
     }
 
-
     @Override
     public void onMessage(Message message, byte[] pattern) {
 
         String channel = new String(message.getChannel());
 
-        List<RedisListenerDetails> redisListeners =  getListeners(channel);
+        List<RedisListenerDetails> redisListeners = getListeners(channel);
 
         redisListeners.forEach(redisListenerDetails -> {
             parseAndInvokeListener(message, redisListenerDetails, channel);
-
         });
-
     }
 
     private void parseAndInvokeListener(Message message, RedisListenerDetails redisListenerDetails, String channel) {
         try {
             Object deserialized = objectMapper.readValue(message.getBody(), redisListenerDetails.getParameterType());
             redisListenerDetails.getMethod().invoke(redisListenerDetails.getBean(), deserialized);
-        }catch (Exception e){
-            log.error("Error while invoking listener for channel {}, payload: {}",channel,new String(message.getBody()), e);
+        } catch (Exception e) {
+            log.error(
+                    "Error while invoking listener for channel {}, payload: {}",
+                    channel,
+                    new String(message.getBody()),
+                    e);
         }
     }
-
-
 }
